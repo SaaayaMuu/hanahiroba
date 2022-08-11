a9efb61e650eeb0fb76c1d9a408dbb8c87ba2fa5
SaaayaMuu <saya322.coco@gmail.com>
2022-08-11 15:29:09 +0000

    [Add]top画面レイアウト

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,7 +1,19 @@
 
+$(function () {
+  function end_loader() {
+    $('.loader').fadeOut(800);
+  }
+  $(window).on('load', function () {
+    setTimeout(function () {
+      end_loader();
+    }, 3000)
+  })
+})
+
+
 //スライドショー
 $(document).ready(function () {
-  $("#theTarget").skippr({
+  $(".theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
     transition : 'fade',
     // 変化に係る時間(ミリ秒)
@@ -9,19 +21,27 @@ $(document).ready(function () {
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
-    navType : 'block',
+    navType : 'false',
     // 子要素の種類('div' or 'img')
     childrenElementType : 'div',
     // ナビゲーション矢印の表示(trueで表示)
-    arrows : true,
+    arrows : false,
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 3000,
+    autoPlayDuration : 2000,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
+  });
+});
+
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
   });
 });
 
@@ -73,4 +93,5 @@ window.onload = function(){
         console.log("Your browser does not support File API");
     }
 }
+
 
