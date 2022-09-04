4462c7206876353cf7b12c6a3a0f8cd9940a9a1c
SaaayaMuu <saya322.coco@gmail.com>
2022-09-04 18:13:45 +0000

    [Add]在庫数を上回る数がカートに入るとエラー

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,13 +11,16 @@ class Public::CartItemsController < ApplicationController
     @cart_items = current_customer.cart_items.all #render用
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      if params[:cart_item][:amount].to_i >= @cart_item.item.stock #if カート内商品のamountが商品のstockを上回ったら
-        flash.now[:notice] = "※在庫数が足りません"
-        render :index
-      else
-        @cart_item.update(amount: @cart_item.amount + params[:cart_item][:amount].to_i)
+      @cart_item.update(amount: @cart_item.amount + params[:cart_item][:amount].to_i)# 1. 一度amountを更新
+
+      if @cart_item.amount > @cart_item.item.stock # 2. もしamountがstockより多くなってしまったら
+        @cart_item.update(amount: @cart_item.item.stock) # 3. amountをstockと同じ数し、再度update
+        flash[:notice] = "※在庫数が足りません"
         redirect_to cart_items_path
+      elsif @cart_item.amount <= @cart_item.item.stock # 2. もしamountがstockの数より少なければ問題ないので
+        redirect_to cart_items_path # 3. そのままredirect
       end
+
 		else
 		  @cart_item = CartItem.new(cart_item_params)
 		  @cart_item.customer_id = current_customer.id
@@ -28,12 +31,13 @@ class Public::CartItemsController < ApplicationController
 
   def update
     @cart_item = CartItem.find(params[:id])
-    if @cart_item.amount >= @cart_item.item.stock #if カート内商品のamountが商品のstockを上回ったら
+    @cart_item.update(cart_item_params)# 1. 一度amountを更新
+    if @cart_item.amount > @cart_item.item.stock # 2. もしamountがstockより多くなってしまったら
+      @cart_item.update(amount: @cart_item.item.stock) # 3. amountをstockと同じ数し、再度update
       flash[:notice] = "※在庫数が足りません"
       redirect_to cart_items_path
-    else
-    @cart_item.update(cart_item_params)
-    redirect_to cart_items_path
+    elsif @cart_item.amount <= @cart_item.item.stock # 2. もしamountがstockの数より少なければ問題ないので
+      redirect_to cart_items_path # 3. そのままredirect
     end
   end
 
