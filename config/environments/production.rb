e287177e3ad934605d610f015b1c5608f393d87b
SaaayaMuu <saya322.coco@gmail.com>
2022-10-15 08:31:36 +0000

    [Fix]production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -141,9 +141,10 @@ Rails.application.configure do
   # config.action_mailer.perform_caching = false
 
   # config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '54.163.14.146' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
