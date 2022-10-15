72d8d187615d565a600711819aa14fb50e876041
SaaayaMuu <saya322.coco@gmail.com>
2022-10-15 14:47:43 +0000

    [Fix]レイアウト

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -9,6 +9,7 @@
 //= link public/orders_index.css
 //= link public/orders_new.css
 //= link public/orders_show.css
+//= link public/orders_thanks.css
 //= link public/registrations_new.css
 //= link public/sessions_new.css
 //= link public/reviews_new.css
