4d124c4fa491cd16d73c3b74604e52e101ecdf9a
SaaayaMuu <saya322.coco@gmail.com>
2022-10-15 05:06:08 +0000

    [Fix]designs#show

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -16,11 +16,11 @@ class Public::ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    if params[:review][:rate] == "nil"
+    @review.save
+    if params[:review][:rate] == nil
       @design = Design.find(params[:design_id])
       render :new
     else
-     @review.save
      redirect_to reviews_path(design_id: @review.design_id)#or (design_id: params[:review][:design_id])でもOK
     end
   end
